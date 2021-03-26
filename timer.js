72f809d8d568bd1d19a61b29a1ba66095f721ea4
AceOfHeart5 <evanconwaymusic@gmail.com>
2021-03-26 01:57:32 -0400

    add sound and fix timer logic

--- timer.js
+++ timer.js
@@ -1,93 +1,65 @@
-const time = {
-    current: {
-        min: 0,
-        sec: 0,
-        ten: 0
-    },
-    set: {
-        min: 25
-    },
-    counting: false
-}
-
-time.current.min = time.set.min;
-
-const decreaseTime = function() {
-    let hitZero = false;
-
-    time.current.ten -= 1;
-
-    if (time.current.ten < 0) {
-        time.current.sec -= 1;
-        time.current.ten = 9;
-    }
-
-    if (time.current.sec < 0) {
-        time.current.min -= 1
-        time.current.sec = 59;
-    }
-
-    if (time.current.min < 0) {
-        time.current.min = 0;
-        time.current.sec = 0;
-        time.current.ten = 0;
-        hitZero = true;
-    }
-
-    return hitZero;
-}
-
 const display = document.getElementById('time-display');
+const buttonStart = document.getElementById('btn-start');
+const buttonReset = document.getElementById('btn-reset');
 
-const updateDisplay = function() {
-    let result = '';
-    result += time.current.min.toLocaleString('en-US', {
-        minimumIntegerDigits: 2,
-    });
-    result += ":";
-    result += time.current.sec.toLocaleString('en-US', {
-        minimumIntegerDigits: 2,
-    });
-    result += '.';
-    result += time.current.ten;
-    display.innerHTML = result;
-};
+// time is stored in milliseconds
+const time = {
+    current: 0,
+    set: 25 * 60 * 1000,
+    target: 0,
+    countDown: false
+}
 
-updateDisplay();
+const displayUpdate = function() {
+    let temp = time.current;
+    const minutes = Math.floor(temp / 60 / 1000);
+    temp -= minutes * 60 * 1000;
+    const seconds = Math.floor(temp / 1000);
+    temp -= seconds * 1000;
+    const milliseconds = temp;
+
+    const min = minutes.toLocaleString('en-US', {
+        minimumIntegerDigits: 2
+    });
+    const sec = seconds.toLocaleString('en-US', {
+        minimumIntegerDigits: 2
+    });
+    const mil = milliseconds.toLocaleString('en-US', {
+        minimumIntegerDigits: 3
+    });
+    display.innerHTML = `${min}:${sec}.${mil}`;
+}
+
+const timeReset = function() {
+    console.log('time reset');
+    time.current = time.set;
+    time.target = new Date().getTime() + time.current;
+    displayUpdate();
+}
+
+timeReset();
+
+const timeUpdate = function() {
+    const check = new Date().getTime();
+    time.current = time.target - check;
+}
 
 setInterval(function() {
-    if (time.counting) {
-        decreaseTime();
-        updateDisplay();
+    if (time.countDown) {
+        timeUpdate();
+        displayUpdate();
     }
 }, 100);
 
-const timerStart = function() {
-    time.counting = true;
-    buttonStart.innerHTML = 'pause';
-}
-
-const timerPause = function() {
-    time.counting = false;
-    buttonStart.innerHTML = 'start';
-}
-
-const buttonStart = document.getElementById('btn-start');
-
 buttonStart.onclick = function() {
-    if (time.counting) {
-        timerPause();
+    if (time.countDown) {
+        time.countDown = false;
+        buttonStart.innerHTML = 'start';
     } else {
-        timerStart();
+        time.countDown = true;
+        buttonStart.innerHTML = 'pause';
+        time.target = new Date().getTime() + time.current;
     }
-};
-
-const buttonReset = document.getElementById('btn-reset');
-
-buttonReset.onclick = function() {
-    timerPause();
-    time.current.min = time.set.min;
-    time.current.sec = 0;
-    time.current.ten = 0;
-    updateDisplay();
 }
+
+buttonReset.onclick = timeReset;
