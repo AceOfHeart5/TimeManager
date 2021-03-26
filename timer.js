cbd0cc5afa7dbd12ccf78884b29983344f0afcb1
AceOfHeart5 <evanconwaymusic@gmail.com>
2021-03-25 23:36:39 -0400

    add better timer logic, still not consistent

--- timer.js
+++ timer.js
@@ -1,20 +1,43 @@
-const speed = 1000;
-const display = document.getElementById('time-display');
-const buttonStart = document.getElementById('btn-start');
-const buttonStop = document.getElementById('btn-stop');
-
-let countingDown = false;
-
 const time = {
     current: {
         min: 0,
-        sec: 0
+        sec: 0,
+        ten: 0
     },
     set: {
-        min: 25,
-        sec: 0
-    }
+        min: 25
+    },
+    counting: false
 }
+
+time.current.min = time.set.min;
+
+const decreaseTime = function() {
+    let hitZero = false;
+
+    time.current.ten -= 1;
+
+    if (time.current.ten < 0) {
+        time.current.sec -= 1;
+        time.current.ten = 9;
+    }
+
+    if (time.current.sec < 0) {
+        time.current.min -= 1
+        time.current.sec = 59;
+    }
+
+    if (time.current.min < 0) {
+        time.current.min = 0;
+        time.current.sec = 0;
+        time.current.ten = 0;
+        hitZero = true;
+    }
+
+    return hitZero;
+}
+
+const display = document.getElementById('time-display');
 
 const updateDisplay = function() {
     let result = '';
@@ -25,54 +48,46 @@ const updateDisplay = function() {
     result += time.current.sec.toLocaleString('en-US', {
         minimumIntegerDigits: 2,
     });
+    result += '.';
+    result += time.current.ten;
     display.innerHTML = result;
+};
+
+updateDisplay();
+
+setInterval(function() {
+    if (time.counting) {
+        decreaseTime();
+        updateDisplay();
+    }
+}, 100);
+
+const timerStart = function() {
+    time.counting = true;
+    buttonStart.innerHTML = 'pause';
 }
 
-const decreaseTime = function() {
-    let hitZero = false;
-
-    time.current.sec -= 1;
-
-    if (time.current.sec < 0) {
-        time.current.min -= 1
-        time.current.sec = 59;
-    }
-
-    if (time.current.min < 0) {
-        time.current.min = 0;
-        time.current.sec = 0;
-        hitZero = true;
-    }
-
-    return hitZero;
+const timerPause = function() {
+    time.counting = false;
+    buttonStart.innerHTML = 'start';
 }
 
-const tick = function () {
-    if (!countingDown) return;
-    let done = decreaseTime();
-    if (done) {
-        countingDown = false;
-    } else {
-        setTimeout(tick, speed);
-    }
-    updateDisplay();
-}
+const buttonStart = document.getElementById('btn-start');
 
 buttonStart.onclick = function() {
-    console.log(countingDown);
-    if (countingDown === true) {
-        buttonStart.innerHTML = 'start';
-        countingDown = false;
+    if (time.counting) {
+        timerPause();
     } else {
-        buttonStart.innerHTML = 'pause';
-        countingDown = true;
-        tick();
+        timerStart();
     }
-}
-
-buttonStop.onclick = function() {
-    countingDown = false;
-    time.current.min = time.set.min;
-    time.current.sec = time.set.sec;
-    updateDisplay();
 };
+
+const buttonReset = document.getElementById('btn-reset');
+
+buttonReset.onclick = function() {
+    timerPause();
+    time.current.min = time.set.min;
+    time.current.sec = 0;
+    time.current.ten = 0;
+    updateDisplay();
+}
